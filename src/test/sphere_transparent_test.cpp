5aff33a966a05f46d1a74fb46d1e00d2152ca56e
jbrandmeyer <jbrandmeyer>
2004-07-08 02:02:49 +0000

    Excersice support for texturing and transparency in the same body.

--- src/test/sphere_transparent_test.cpp
+++ src/test/sphere_transparent_test.cpp
@@ -1,4 +1,5 @@
 #include "gtk2/render_surface.hpp"
+#include "gtk2/file_texture.hpp"
 #include "sphere.hpp"
 
 int 
@@ -6,10 +7,13 @@ main( void)
 {
 	basic_app main_window( "Sphere transparency test");
 	
+	shared_ptr<texture> universe = file_texture::create( 
+		"/home/jonathan/Projects/vpython-core2/data/2mass_allskyatlas.gif");
 	// Draw two yellow spheres, one transparent.
 	shared_ptr<sphere> sph( new sphere(4));
 	sph->set_pos( vector(1, -1, 0) * 2);
-	sph->set_color( rgba( 0.2, 0.2, 1, 0.3));
+	sph->set_color( rgba( 1, 1, 1, 0.3));
+	sph->set_texture( universe);
 	
 	shared_ptr<sphere> sph2( new sphere(4));
 	sph2->set_pos( vector(1, 1, 0) * 2);
@@ -18,6 +22,7 @@ main( void)
 
 	main_window.scene.add_renderable( sph);
 	main_window.scene.add_renderable( sph2);
+	main_window.scene.core.background = rgba();
 
 	main_window.run();
 	return 0;
