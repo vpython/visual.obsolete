71d6021e7bc67033f2075e3b4d518fcb3b1860d9
jbrandmeyer <jbrandmeyer>
2004-07-09 17:16:16 +0000

    Excersize the new translucent+textured sphere code.

--- src/test/sphere_texture_test.cpp
+++ src/test/sphere_texture_test.cpp
@@ -11,7 +11,7 @@ main( int, char** )
 	// The coordinates of Chapel Hill, NC.
 	double lat = 35.0 + 46.0 / 60.0;
 	double lng = -(79.0 + 4.0 / 60.0);
-	tmatrix horiz_rot = rotation( lng * G_PI / 180.0, vector(0,1,0));
+	tmatrix horiz_rot = rotation( lng * G_PI / 180.0, vector(0, 1, 0));
 	// Apply me first.
 	tmatrix vert_rot = rotation( lat * G_PI / 180.0, vector(0, 0, 1));
 	vector axis( 1, 0, 0);
@@ -20,19 +20,15 @@ main( int, char** )
 	axis = -axis * 0.2;
 	
 	// Load up a map of the earth.
-	shared_ptr<texture> earth = file_texture::create( "/home/jonathan/Projects/"
-		"vpython_basic_gtkmm/data/land_ocean_ice_2048.tif");
+	shared_ptr<texture> earth = file_texture::create( "/home/jdbrandm/workspace/"
+		"vpython-core2/data/land_ocean_ice_2048.tif");
 	
 	// Draw one sphere with a map of the earth.
-	shared_ptr<sphere> sph( new sphere(3));
+	shared_ptr<sphere> sph( new sphere());
 	sph->set_texture( earth);
-	sph->set_radius( 1);
-	
-	// Draw a slightly larger sphere with very faint transparent blue :)
-	shared_ptr<sphere> haze( new sphere(3));
-	haze->set_color( rgba( .2, .2, 1, .3));
-	haze->set_radius( 1.05);
-	
+	sph->set_color( rgba( 1, 1, 1, 0.8));
+
+	// Draw one translucent arrow pointing at us :)	
 	shared_ptr<arrow> arr( new arrow());
 	arr->set_pos( pos);
 	arr->set_axis( axis);
