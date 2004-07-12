bb81df97d81595c85c8569039108e65666e000db
jbrandmeyer <jbrandmeyer>
2004-07-12 20:06:51 +0000

    Use default constructor.

--- src/test/sphere_lod_test.cpp
+++ src/test/sphere_lod_test.cpp
@@ -7,26 +7,26 @@ main( void)
 	basic_app main_window( "Sphere level of detail test");
 	
 	// Draw 4 spheres with each level of detail.
-	shared_ptr<sphere> sph( new sphere(0));
+	shared_ptr<sphere> sph( new sphere());
 	sph->set_pos( vector(1, -1, 0) * 2);
 	sph->set_color( rgba( 1, 1, 0));
 	sph->set_shininess( 0.99);
-	shared_ptr<sphere> sph2( new sphere(1));
+	shared_ptr<sphere> sph2( new sphere());
 	sph2->set_pos( vector(1, 1, 0) * 2);
 	sph2->set_color( rgba( 1, 1, 0));
 	sph2->set_shininess( 0.7);
-	shared_ptr<sphere> sph3( new sphere(2));
+	shared_ptr<sphere> sph3( new sphere());
 	sph3->set_pos( vector(-1, 1, 0) * 2);
 	sph3->set_color( rgba( 1, 1, 0));
 	sph3->set_shininess( 0.5);
-	shared_ptr<sphere> sph4( new sphere(3));
+	shared_ptr<sphere> sph4( new sphere());
 	sph4->set_pos( vector(-1, -1, 0) * 2);
 	sph4->set_color( rgba(1, 1, 0));
 	sph4->set_shininess( 0.2);
-	shared_ptr<sphere> sph5( new sphere(4));
+	shared_ptr<sphere> sph5( new sphere());
 	sph5->set_pos( vector(-1, -1, 0) * 4);
 	sph5->set_color( rgba(1, 1, 0));
-	shared_ptr<sphere> sph6( new sphere(5));
+	shared_ptr<sphere> sph6( new sphere());
 	sph6->set_pos( vector(-1, -1, 0) * 6);
 	sph6->set_color( rgba(1, 1, 0));
 	
