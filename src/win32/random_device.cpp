d116c77307a940a32561ce86aaad95c683aab1e2
jbrandmeyer <jbrandmeyer>
2004-07-31 19:09:40 +0000

    Add a random number generator backend for Win32.

---
+++ src/win32/random_device.cpp
@@ -0,0 +1,92 @@
+/* boost random_device.cpp implementation
+ *
+ * Copyright Jens Maurer 2000
+ * Permission to use, copy, modify, sell, and distribute this software
+ * is hereby granted without fee provided that the above copyright notice
+ * appears in all copies and that both that copyright notice and this
+ * permission notice appear in supporting documentation,
+ *
+ * Jens Maurer makes no representations about the suitability of this
+ * software for any purpose. It is provided "as is" without express or
+ * implied warranty.
+ *
+ * $Id: random_device.cpp,v 1.1 2004/07/31 19:09:40 jbrandmeyer Exp $
+ *
+ */
+ 
+/* Changes:
+ *    modify check for the availability of /dev/urandom to include OSX. -JDB
+ *    Replace impl with a version that uses the MS Crypto API to generate random
+ *       numbers for use on MS Windows. -JDB
+ * 
+ */
+
+#include <boost/nondet_random.hpp>
+#include <string>
+#include <cassert>
+
+
+#ifndef BOOST_NO_INCLASS_MEMBER_INITIALIZATION
+//  A definition is required even for integral static constants
+const bool boost::random_device::has_fixed_range;
+const boost::random_device::result_type boost::random_device::min_value;
+const boost::random_device::result_type boost::random_device::max_value;
+#endif
+
+#define WIN32_LEAN_AND_MEAN 1
+#include <windows.h>
+#include <wincrypt.h>
+
+class boost::random_device::impl
+{
+ public:
+	impl()
+		: handle(0)
+	{
+		if (!CryptAquireContext( &handle, 0, 0, PROV_RSA_FULL, CRYPT_VERIFYCONTEXT))
+			error();
+	}
+	
+	~impl()
+	{
+		CryptReleaseContext( handle, 0);
+	}
+	
+	unsigned int next()
+	{
+		unsigned int ret = 0;
+		CryptGenRandom( handle, sizeof unsigned int, (BYTE*) *ret);
+		assert(ret != 0);
+		return ret;
+	}
+
+ private:
+	HCRYPTPROV handle;
+	error()
+	{
+		throw std::runtime_error( "CryptAquireContext() failed");
+	}
+};
+
+boost::random_device::random_device(const std::string&)
+  : pimpl(new impl())
+{
+  assert(std::numeric_limits<result_type>::max() == max_value);
+}
+
+boost::random_device::~random_device()
+{
+  // the complete class impl is now visible, so we're safe
+  // (see comment in random.hpp)
+  delete pimpl;
+}
+
+double boost::random_device::entropy() const
+{
+  return 10;
+}
+
+unsigned int boost::random_device::operator()()
+{
+  return pimpl->next();
+}
