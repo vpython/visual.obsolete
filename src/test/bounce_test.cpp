a4e2e6527700433b81f50f95bbdee9a39577a2d5
jbrandmeyer <jbrandmeyer>
2004-08-13 00:27:01 +0000

    Modify this test to excesize multiple displays.

--- src/test/bounce_test.cpp
+++ src/test/bounce_test.cpp
@@ -5,6 +5,7 @@
 #include "display.hpp"
 #include "sphere.hpp"
 #include "box.hpp"
+#include "util/rate.hpp"
 
 #include <iostream>
 
@@ -14,6 +15,7 @@ int
 realmain( std::vector<std::string>&)
 {
 	display app;
+	display app2;
 	app.set_title( "Ellipsoid test");
 	rgba red( 1, 0, 0);
 	rgba blue( 0, 0, 1);
@@ -29,14 +31,20 @@ realmain( std::vector<std::string>&)
 	ball->set_color( red);
 	app.add_renderable( ball);
 	
+	app2.add_renderable( shared_ptr<sphere>( new sphere()));
+	
 	vector velocity(0, -1, 0);
-	double dt = 0.000001;
+	double dt = 0.04;
 	while (!gui_main::allclosed()) {
+		rate( 1/dt);
 	    ball->get_pos() += velocity*dt;
 	    if (ball->get_y() < 1)
 	        velocity.y = -velocity.y;
 	    else
 	        velocity.y -= 9.8*dt;
+		while (app.get_kb().size()) {
+			std::cout << app.get_kb().pop() << "\n";
+		}
 	}
 	return 0;
 }
