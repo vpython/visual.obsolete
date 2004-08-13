ee3a1e370683637cd644351f2f52611891e9b5f2
jbrandmeyer <jbrandmeyer>
2004-08-13 00:26:28 +0000

    Make the time difference functions correct.

--- src/test/sleep_test.cpp
+++ src/test/sleep_test.cpp
@@ -25,6 +25,28 @@
 // A request between 1-15 causes a short delay of 300-900 usec.  A second call
 // immediately after the first introduces a full 16 ms wait.
 
+// A convenience function to take the difference between two timeval's.
+timeval 
+operator-( const timeval& lhs, const timeval& rhs)
+{
+	timeval ret = { 
+		lhs.tv_sec - rhs.tv_sec,
+		lhs.tv_usec - rhs.tv_usec
+	};
+	if (ret.tv_usec >= 1000000) {
+		ret.tv_sec++;
+		ret.tv_usec -= 1000000;
+	}
+	return ret;
+}
+
+// A convenience function to print a timeval.
+std::ostream&
+operator<<( std::ostream& stream, const timeval& time)
+{
+	stream << time.tv_sec * 1000000 + time.tv_usec << " usec";
+}
+
 int
 main(int argc, char** argv) 
 {
@@ -52,6 +74,7 @@ main(int argc, char** argv)
 	std::cout << "Sleep delay: " << (end - begin) * 1000000 / tics_per_second << " usec.\n";
 
 #else
+#if 0
 	sched_param pri;
 	pri.sched_priority = 1;
 	sched_setscheduler( 0, SCHED_RR, &pri);
@@ -64,6 +87,7 @@ main(int argc, char** argv)
 	else if (errno == ESRCH) {
 		std::cerr << "Could not set scheduling policy: pid not found.\n";
 	}
+#endif
 	
 	timespec sleep_time = { 0, ms * 1000000};
 	timespec remain = { 0, 0};
@@ -75,18 +99,18 @@ main(int argc, char** argv)
 	
 	gettimeofday( &begin, 0);
 	gettimeofday( &end, 0);
-	long timer_delay = end.tv_usec - begin.tv_usec;	
+	timeval timer_delay = end - begin;
 	timerclear( &begin);
 	timerclear( &end);
 	
-	std::cout << "Clock latency: " << timer_delay << " usec.\n";
+	std::cout << "Clock overhead: " << timer_delay << ".\n";
 	std::cout << "Requested delay: " << sleep_time.tv_nsec / 1000 << " usec.\n";
 	
 	gettimeofday( &begin, 0);
 	nanosleep( &sleep_time, &remain);
 	gettimeofday( &end, 0);
 	
-	std::cout << "nanosleep delay: " << end.tv_usec - begin.tv_usec << " usec with "
+	std::cout << "nanosleep delay: " << end - begin << " with "
 		<< remain.tv_nsec / 1000 << " usec remaining.\n";
 	std::cout << "Claimed resolution: " << 1.0/HZ * 1e6 << " usec.\n";
 	timerclear( &begin);
@@ -95,7 +119,7 @@ main(int argc, char** argv)
 	gettimeofday( &begin, 0);
 	usleep( ms * 1000);
 	gettimeofday( &end, 0);
-	std::cout << "usleep delay: " << end.tv_usec - begin.tv_usec << " usec.\n";
+	std::cout << "usleep delay: " << end - begin << ".\n";
 #endif
 
 	return 0;
