754bcbbf485fa8c586a4ee61d098897ecd5f2d0b
jbrandmeyer <jbrandmeyer>
2004-08-13 00:44:11 +0000

    Fix copyright statements.

--- src/python/wrap_rgba.cpp
+++ src/python/wrap_rgba.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2000, 2001, 2002, 2003 by David Scherer and others.
+
 // Copyright (c) 2003, 2004 by Jonathan Brandmeyer and others.
 // See the file license.txt for complete license terms.
 // See the file authors.txt for a complete list of contributors.
--- src/python/wrap_vector.cpp
+++ src/python/wrap_vector.cpp
@@ -1,6 +1,5 @@
 // This file currently requires 196 MB to compile (optimizing).
 
-// Copyright (c) 2000, 2001, 2002, 2003 by David Scherer and others.
 // Copyright (c) 2003, 2004 by Jonathan Brandmeyer and others.
 // See the file license.txt for complete license terms.
 // See the file authors.txt for a complete list of contributors.
