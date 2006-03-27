c02864b08314c5e84fcef9d500ae890a8e9e8e8d
jbrandmeyer <jbrandmeyer>
2006-03-27 04:58:33 +0000

    Port to Gtkmm 2.4

--- include/util/gl_free.hpp
+++ include/util/gl_free.hpp
@@ -8,7 +8,7 @@ namespace cvisual {
 
 // When the last open OpenGL window is closed, this signal will be emitted to
 // allow any texture and displaylist resources to be freed.
-extern SigC::Signal0<void> on_gl_free;
+extern sigc::signal0<void> on_gl_free;
 	
 } // !namespace cvisual
 
--- src/core/util/gl_free.cpp
+++ src/core/util/gl_free.cpp
@@ -1,5 +1,5 @@
 #include "util/gl_free.hpp"
 
 namespace cvisual {
-SigC::Signal0<void> on_gl_free;	
+sigc::signal0<void> on_gl_free;	
 }
