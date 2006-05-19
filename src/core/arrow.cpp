f0e8f50b753d4eed10265f642493cb054a265cb2
jbrandmeyer <jbrandmeyer>
2006-05-19 17:32:52 +0000

    Silence warning

--- src/core/arrow.cpp
+++ src/core/arrow.cpp
@@ -96,7 +96,7 @@ arrow::recalc_sorted_model( const double& gcf)
 }
 
 void
-arrow::cache_transparent_model( const view& scene)
+arrow::cache_transparent_model( const view&)
 {
 	assert( color.alpha < 1.0);
 	// Compile the rendering code for the beastie.
