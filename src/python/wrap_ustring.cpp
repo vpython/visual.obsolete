f9dfd0bc4b3a1622249ae394a3af8eee5f9771c4
jbrandmeyer <jbrandmeyer>
2006-05-30 17:28:54 +0000

    More windows support; Find .glade files and such appropriately

--- src/python/wrap_ustring.cpp
+++ src/python/wrap_ustring.cpp
@@ -6,8 +6,11 @@
 
 #include <glibmm/init.h>
 #include <glibmm/convert.h>
+#include <glibmm/thread.h>
 
 #include <string>
+#include <iostream>
+#include <cstdlib>
 
 namespace cvisual {
 
@@ -86,21 +89,38 @@ wrap_glib_ustring(void)
 	std::string pyunicode_encoding;
 	switch (pyunicode_size) {
 		case 1:
-			pyunicode_encoding = "UTF8";
+			pyunicode_encoding = "UTF-8";
 			break;
 		case 2:
-			pyunicode_encoding = "UTF16";
+			pyunicode_encoding = "UTF-16";
 			break;
 		case 4:
-			pyunicode_encoding = "UTF32";
+			pyunicode_encoding = "UTF-32";
 			break;
 		default:
-			pyunicode_encoding = "UTF8";
+			pyunicode_encoding = "UTF-8";
 	}
 	
 	Glib::init();
-	utf8_to_utf16 = new Glib::IConv( pyunicode_encoding, "UTF8");
-	utf16_to_utf8 = new Glib::IConv( "UTF8", pyunicode_encoding);
+	if (!Glib::thread_supported())
+		Glib::thread_init();
+	
+	try {
+	utf8_to_utf16 = new Glib::IConv( pyunicode_encoding, "UTF-8");
+	} catch (Glib::ConvertError& e) {
+		std::cerr << "Failed creating converter from UTF-8 to "
+			<< pyunicode_encoding << ": " << e.what() << std::endl;
+	}
+	try {
+	utf16_to_utf8 = new Glib::IConv( "UTF-8", pyunicode_encoding);
+	} catch (Glib::ConvertError& e) {
+		std::cerr << "Faile creating converter from " << pyunicode_encoding
+			<< " to UTF8: " << e.what() << std::endl;
+	}
+	if (!(utf8_to_utf16 && utf16_to_utf8)) {
+		std::cerr << "Aborting." << std::endl;
+		std::exit(1);
+	}
 	
 	glib_ustring_from_pyunicode();
 	py::to_python_converter< Glib::ustring, glib_ustring_to_pyunicode>();
