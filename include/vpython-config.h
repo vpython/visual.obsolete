f8223e5064f55dde802c9fcfc081cb7565742abe
jbrandmeyer <jbrandmeyer>
2006-05-31 19:03:04 +0000

    elide unused macro

--- include/vpython-config.h
+++ include/vpython-config.h
@@ -1,10 +1,10 @@
 #ifndef VPYTHON_CONFIG_H
 #define VPYTHON_CONFIG_H
 
+#include <math.h>
+
 #ifndef M_PI
 # define M_PI 3.14159265359
 #endif
-
-#define VPYTHON_PREFIX "/home/jdbrandm/workspace/vpython-core2"
 
 #endif
