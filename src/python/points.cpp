b7bc381c0d1b9c0c2ebc312082806fef50e27fe3
jbrandmeyer <jbrandmeyer>
2006-06-05 17:00:46 +0000

    Some cleanup; Make world-space points work correctly

--- src/python/points.cpp
+++ src/python/points.cpp
@@ -5,9 +5,12 @@
 #include "util/errors.hpp"
 #include "util/gl_enable.hpp"
 
-#include <set>
+#include "wrap_gl.hpp"
+
 #include <vector>
-#include <iostream>
+#include <sstream>
+#include <algorithm>
+#include <set>
 
 namespace cvisual { namespace python {
 
@@ -31,14 +34,46 @@ double gl_aliased_radius_range[2] = {-1, -1};
 double gl_smooth_radius_range[2] = {-1, -1};
 int world_scale_points_supported = -1;
 
+#if defined( _WIN32) || defined (_MSC_VER)
+void default_glPointParameterfvARB( GLenum, const GLfloat*)
+{
+	return;
+}
+PFNGLPOINTPARAMETERFVARBPROC glPointParameterfvARB = 
+	(PFNGLPOINTPARAMETERFVARBPROC)default_glPointParameterfvARB;
+#endif
+
+void 
+init_pointparam_extension(void)
+{
+	// Determine if GLPointParameters is supported
+	using namespace std;
+	set<string> extensions;
+	istringstream strm( string( (const char*)(glGetString( GL_EXTENSIONS))));
+	copy( istream_iterator<string>(strm), istream_iterator<string>(), 
+		inserter( extensions, extensions.begin()));
+	if (extensions.find("GL_ARB_point_parameters") != extensions.end()) {
+		VPYTHON_NOTE( "Using GL_ARB_point_parameters extension");
+		world_scale_points_supported = 1;
+		#if defined( _WIN32) || defined (_MSC_VER)
+		glPointParameterfvARB = (PFNGLPOINTPARAMETERFVARBPROC)wglGetProcAddress( "glPointParameterfvARB");
+		#endif
+	}
+	else
+		world_scale_points_supported = 0;
+	
+	glGetDoublev( GL_ALIASED_POINT_SIZE_RANGE, gl_aliased_radius_range);
+	glGetDoublev( GL_SMOOTH_POINT_SIZE_RANGE, gl_smooth_radius_range);
+}
+
 } // !namespace (anon)
 
 
 points::points()
 	: pos(0), color(0), preallocated_size(256), count(0), 
 	size_type(SCREEN), 
-	antialias(false),
-	size( 1)
+	antialias(true),
+	size( 1.5)
 {
 	std::vector<int> dims(2);
 	dims[0] = preallocated_size;
@@ -524,10 +559,8 @@ points::gl_render( const view& scene)
 	if (degenerate())
 		return;
 	
-	std::multiset< point_coord, face_z_comparator> translucent_points(
-		face_z_comparator( scene.forward));
-	typedef std::multiset< point_coord, face_z_comparator>::iterator
-		translucent_iterator;
+	std::vector<point_coord> translucent_points;
+	typedef std::vector<point_coord>::iterator translucent_iterator;
 	
 	std::vector<point_coord> opaque_points;
 	typedef std::vector<point_coord>::iterator opaque_iterator;
@@ -538,16 +571,16 @@ points::gl_render( const view& scene)
 	const float* color_i = findex( color, 0);
 	const float* color_end = findex( color, count);
 
-	// First classify each point based on whether or not it must be transparent
+	// First classify each point based on whether or not it is translucent
 	if (antialias) { // Every point must be depth sorted
 		for ( ; pos_i < pos_end && color_i < color_end; pos_i += 3, color_i += 4) {
-			translucent_points.insert( point_coord( vector(pos_i)*scene.gcf, rgba(color_i)));
+			translucent_points.push_back( point_coord( vector(pos_i), rgba(color_i)));
 		}
 	}
 	else { // Only translucent points need to be depth-sorted
 		for ( ; pos_i < pos_end && color_i < color_end; pos_i += 3, color_i += 4) {
 			if (color_i[3] < 1.0)
-				translucent_points.insert( point_coord( vector(pos_i), rgba(color_i)));
+				translucent_points.push_back( point_coord( vector(pos_i), rgba(color_i)));
 			else
 				opaque_points.push_back( point_coord( vector(pos_i), rgba(color_i)));
 		}
@@ -559,9 +592,9 @@ points::gl_render( const view& scene)
 		for (opaque_iterator i = opaque_points.begin(); i != opaque_points.end(); ++i) {
 			i->center *= scene.gcf;
 		}
-		// The translucent points are always multiplied by the gcf, since doing
-		// otherwise would require re-creation of the container (can't change
-		// the points out from under the sorting criterion).	
+		for (translucent_iterator i = translucent_points.begin(); i != translucent_points.end(); ++i) {
+			i->center *= scene.gcf;
+		}
 	}
 	if (scene.anaglyph) {
 		if (scene.coloranaglyph) {
@@ -581,50 +614,97 @@ points::gl_render( const view& scene)
 			}		
 		}
 	}
+	// Sort the translucent points
+	if (!translucent_points.empty()) {
+		std::stable_sort( translucent_points.begin(), translucent_points.end(), 
+			face_z_comparator(scene.forward));
+	}
 	
 	clear_gl_error();
 	if (world_scale_points_supported < 0) {
-		// Determine if GLPointParameters is supported
-		world_scale_points_supported = 1;
-		glGetDoublev( GL_ALIASED_POINT_SIZE_RANGE, gl_aliased_radius_range);
-		glGetDoublev( GL_SMOOTH_POINT_SIZE_RANGE, gl_smooth_radius_range);
+		init_pointparam_extension();
 	}
-
-#if 0
-	if (size_type == WORLD) {
-#if 1
-		// World-to-eye transformation (modelview matrix)
-		tmatrix mv;
-		modelview.gl_modelview_get();
-		// Eye-to-world transform (inverse of modelview)
-		tmatrix mv_inv;
-		// and its inverse, the eye-to-world transformation
-		inverse( mv_inv, mv);
-		// Find the center of the universe, in eye coordinates
-		vector center = mv * vector();
-		// Its eye distance from the viewer
-		double dist = center.mag();
-		// Find a vector in the direction of the user's right, in world coordinates,
-		// of the same length as the size of the points in this object
-		vector right = (mv_inv * (center + vector(0.1, 0))).norm()*size;
-		// Compute position of that vector in eye coordinates
-		double scale = ((mv * right) - center).mag();
-#else
-		double scale = scene.pixel_coverage(size);
-#endif
-		float attenuation_constants[] = { 0, 0, 
-	}
-#endif
 	
+	if (antialias)
+		glEnable( GL_POINT_SMOOTH);
+
+	if (size_type == WORLD && world_scale_points_supported > 0) {
+		/* The coefficients of the point parameters equation are derived as
+		 * follows:
+		 * 
+		 * Let |x-y| be the size of a unit-size point, such that x is the center
+		 * of a sphere, and y is a to the right, one unit distance away.
+		 * Let x' = modelview * x; x" = projection * x'
+		 * 
+		 * The eye-coordinate distance of x is |x'|
+		 * The screen size of the sphere is equal to |x"-y"|*window_width
+		 * 
+		 * The point attenuation equation is: 
+		 * 	s_result = s_original * 1.0/sqrt(a + b*d + c* d^2)
+		 *  where d = |x'| (eye coordinate distance of the point)
+		 * 
+		 * Since we only desire linear attenuation commisserate with distance
+		 * from the viewer, let a = b = 0
+		 * s_result = s_original * 1.0/(sqrt(c)*d)
+		 * 
+		 * Substitue s_original == 2; s_result == |x"-y"|*window_width; d == |x'|
+		 * |x"-y"|*window_width = 2/(sqrt(c)*|x'|)
+		 * 
+		 * Solve for c:
+		 * c = {2.0/(|x'| * |x"-y"| * window_width)}^2
+		 * 
+		 * Note: There is almost certainly an easier way to get the same number
+		 * from the projection or modelview matrices directly.  Nonetheless, this
+		 * does work, and is implemented below.
+		 */
+		// World-to-eye transformation (modelview matrix)
+		tmatrix mv; mv.gl_modelview_get();
+		// Projection matrix
+		tmatrix proj; proj.gl_projection_get();
+		// Eye-to-world transform (inverse of modelview)
+		tmatrix mv_inv;	inverse( mv_inv, mv);
+		
+		// Find the center of the universe, in eye coordinates
+		vector world_center;
+		vector eye_center = mv * world_center;
+		// Its eye distance from the viewer
+		double dist = eye_center.mag();
+		// Find a vector in the direction of the user's right, in world space,
+		// of unit length
+		vector world_right = (mv_inv * (eye_center + vector(0.1, 0))).norm();
+		// Compute position of the center and right vectors in screen space
+		vector center_screen = proj.project( mv * world_center);
+		vector right_screen = proj.project( mv * world_right);
+		// The size of the sphere at this distance from the camera
+		double scale = (center_screen - right_screen).mag() * scene.window_width * 0.5f;
+		
+		float attenuation_eqn[] = { 0, 0, 1.0f/std::pow( (scale * dist), 2.0) };
+		glPointParameterfvARB( GL_POINT_DISTANCE_ATTENUATION_ARB, attenuation_eqn);
+		glPointSize( size);
+	}
+	else if (size_type == SCREEN) {
+		// Restore to default (aka, disable attenuation)
+		if (world_scale_points_supported > 0) {
+			float attenuation_eqn[] = {1.0f, 0.0f, 0.0f};
+			glPointParameterfvARB( GL_POINT_DISTANCE_ATTENUATION_ARB, attenuation_eqn);
+		}
+		if (antialias) {
+			glPointSize( clamp( gl_smooth_radius_range[0], size, gl_smooth_radius_range[1]));
+		}
+		else {
+			glPointSize( clamp( gl_aliased_radius_range[0], size, gl_aliased_radius_range[1]));
+		}
+	}
+	// Finish GL state prep
 	gl_disable ltg( GL_LIGHTING);
+	gl_enable_client v( GL_VERTEX_ARRAY);
+	gl_enable_client c( GL_COLOR_ARRAY);
+	
 	// Render opaque points (if any)
 	if (opaque_points.size()) {
-		gl_enable_client v( GL_VERTEX_ARRAY);
-		gl_enable_client c( GL_COLOR_ARRAY);
 		const std::ptrdiff_t chunk = 256;
 		opaque_iterator begin = opaque_points.begin();
 		opaque_iterator end = opaque_points.end();
-		glPointSize( clamp( gl_aliased_radius_range[0], size, gl_aliased_radius_range[1]));
 		while (begin < end) {
 			std::ptrdiff_t block = std::min( chunk, end - begin);
 			glColorPointer( 4, GL_FLOAT, sizeof(point_coord), &begin->color.red);
@@ -635,27 +715,24 @@ points::gl_render( const view& scene)
 	}
 	
 	// Render translucent points (if any)
-	if (translucent_points.size()) {
-		if (antialias) {
-			glPointSize( clamp( gl_aliased_radius_range[0], size, gl_aliased_radius_range[1]));
-			glEnable( GL_POINT_SMOOTH);
-		}
-		else {
-			glPointSize( clamp( gl_smooth_radius_range[0], size, gl_smooth_radius_range[1]));
-		}
+	if (!translucent_points.empty()) {
 		gl_enable blend( GL_BLEND);
 		glBlendFunc( GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 		
-		glBegin( GL_POINTS);
-		for (translucent_iterator i = translucent_points.begin(); i != translucent_points.end(); ++i) {
-			i->color.gl_set();
-			i->center.gl_render();
-		}
-		glEnd();
-		
-		if (!antialias) {
-			glDisable( GL_POINT_SMOOTH);
-		}
+		const std::ptrdiff_t chunk = 256;
+		translucent_iterator begin = translucent_points.begin();
+		translucent_iterator end = translucent_points.end();
+		while (begin < end) {
+			std::ptrdiff_t block = std::min( chunk, end - begin);
+			glColorPointer( 4, GL_FLOAT, sizeof(point_coord), &begin->color.red);
+			glVertexPointer( 3, GL_DOUBLE, sizeof(point_coord), &begin->center.x);
+			glDrawArrays( GL_POINTS, 0, block);
+			begin += block;
+		}		
+	}
+	
+	if (!antialias) {
+		glDisable( GL_POINT_SMOOTH);
 	}
 	check_gl_error();
 }
