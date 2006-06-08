0ba73dd55a92e404412f691a2744ceb2c3a93f3b
jbrandmeyer <jbrandmeyer>
2006-06-08 23:02:29 +0000

    Use newly supported short form for an alpha map

--- examples/textext.py
+++ examples/textext.py
@@ -5,10 +5,10 @@ from visual import *
 # 3 is RGB, 4 is RGBA
 # The primitive data type can be a char, int, long, short, float, or double.
 # however, int types are converted from 0-255 -> 0.->1.
-checkerboard = array([[[0.],[0.],[1.],[1.]], 
-					  [[0.],[0.],[1.],[1.]],
-					  [[1.],[1.],[0.],[0.]],
-					  [[1.],[1.],[0.],[0.]]])
+checkerboard = array([[0.,0.,1.,1.], 
+					  [0.,0.,1.,1.],
+					  [1.,1.,0.,0.],
+					  [1.,1.,0.,0.]])
 lum = texture( data=checkerboard, type = "luminance")
 checkerboard = lum.data # in case a copy was made
 # By default, textures are mipmapped automatically.  The user should set it to
