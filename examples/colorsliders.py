cfe201e52f0fb3c8e59cf3353fbd3bd8b7a22456
jbrandmeyer <jbrandmeyer>
2006-06-13 17:03:19 +0000

    Include an alpha slider

--- examples/colorsliders.py
+++ examples/colorsliders.py
@@ -47,18 +47,20 @@ scene.height = 400
 scene.center = (0,0.5,0)
 scene.title = "RGB and HSV color"
 wcube = 0.2
-rgb = (1,0,0)
+rgba = (1,0,0,1)
 hsv = color.rgb_to_hsv((1,0,0))
-ctrl = [slider(pos=(-1.5,0,0), color=(1,0,0), max=1., value=rgb[0]),
-           slider(pos=(-1.25,0,0), color=(0,1,0), max=1., value=rgb[1]),
-           slider(pos=(-1.0,0,0), color=(0,0,1), max=1., value=rgb[2]),
+ctrl = [slider(pos=(-1.75,0,0), color=(1,0,0), max=1., value=rgba[0]),
+           slider(pos=(-1.5,0,0), color=(0,1,0), max=1., value=rgba[1]),
+           slider(pos=(-1.25,0,0), color=(0,0,1), max=1., value=rgba[2]),
+           slider(pos=(-1,0,0), color=(0,0,0),max=1., value=rgba[3]),
         slider(pos=(+1.0,0,0), color=(1,0,0), max=1., value=hsv[0]),
            slider(pos=(+1.25,0,0), color=(1,1,1), max=1., value=hsv[1]),
            slider(pos=(+1.5,0,0), color=(0.5,0.5,0.5), max=1., value=hsv[2])]
-panel = box(pos=(0,0.5,0), length=1.5, height=1, width=0.1, color=rgb)
-ball = sphere(pos=(0,0.5,0), radius=0.5, color=rgb)
+panel = box(pos=(0,0.5,0), length=1.5, height=1, width=0.1, color=rgba)
+ball = sphere(pos=(0,0.5,0), radius=0.5, color=rgba)
 cube = box(pos=(0,1.2,0), axis=(1,1,1),
-           length=wcube, width=wcube, height=wcube, color=rgb)
+           length=wcube, width=wcube, height=wcube, color=rgba)
+behind = arrow( pos=(-.75, 0, -.75), axis=(1.5, 1, 0), color=color.white)
 dragobj = None
 
 while 1:
@@ -67,13 +69,13 @@ while 1:
     if scene.mouse.events:
         m = scene.mouse.getevent()
         if m.click == "left" and m.pick in [panel, ball, cube]:
-            print "RGB = (%0.3f,%0.3f,%0.3f)" % (ctrl[0].value,ctrl[1].value,ctrl[2].value)
-            print "HSV = (%0.3f,%0.3f,%0.3f)" % (ctrl[3].value,ctrl[4].value,ctrl[5].value)
+            print "RGBA = (%0.3f,%0.3f,%0.3f,%0.3f)" % (ctrl[0].value,ctrl[1].value,ctrl[2].value,ctrl[3].value)
+            print "HSV = (%0.3f,%0.3f,%0.3f)" % (ctrl[4].value,ctrl[5].value,ctrl[6].value)
             continue
         elif m.drop == "left":
             dragobj = None
         elif m.drag == "left":
-            for index in range(6):
+            for index in range(7):
                 s = ctrl[index]
                 if m.pick is s.control:
                     pos = m.project(normal=(0,0,1))
@@ -83,14 +85,14 @@ while 1:
     if dragobj and (newpos != pos):
         pos = newpos
         dragobj.getslider(pos)
-        if index <= 2: # rgb sliders
-            cube.color = ball.color = panel.color = (ctrl[0].value,ctrl[1].value,ctrl[2].value)
-            hsv = color.rgb_to_hsv(ball.color)
+        if index <= 3: # rgb sliders
+            cube.color = ball.color = panel.color = (ctrl[0].value,ctrl[1].value,ctrl[2].value,ctrl[3].value)
+            hsv = color.rgb_to_hsv(ball.color[:-1])
             for nn in range(3):
-                ctrl[nn+3].setslider(hsv[nn])
+                ctrl[nn+4].setslider(hsv[nn])
         else: # hsv sliders
-            rgb = color.hsv_to_rgb((ctrl[3].value,ctrl[4].value,ctrl[5].value))
-            cube.color = ball.color = panel.color = rgb
+            rgba = color.hsv_to_rgb((ctrl[4].value,ctrl[5].value,ctrl[6].value)) + (ctrl[3].value,)
+            cube.color = ball.color = panel.color = rgba
             for nn in range(3):
-                ctrl[nn].setslider(rgb[nn])
+                ctrl[nn].setslider(rgba[nn])
 
