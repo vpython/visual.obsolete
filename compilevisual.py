cbc92c0815c185f68791c38a1d83ded5533e43ae
bsherwood <bsherwood>
2006-06-19 20:58:53 +0000

    A helper program for preparing a Windows installer. Run this to compile all the .py files in the visual folder.

---
+++ compilevisual.py
@@ -0,0 +1,9 @@
+from visual.graph import *
+from visual.text import *
+from visual.controls import *
+from visual.factorial import *
+sphere(color=color.red)
+print factorial(4)
+
+# Run this little program to compile all the visual modules.
+# This is part of the production of a VPython installer for Windows.
