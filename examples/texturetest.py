9c58de42bf0d0f781de0e902d53e52eeb40b9a97
bsherwood <bsherwood>
2008-03-05 21:43:27 +0000

    Install code to refresh the textures when they change.

--- examples/texturetest.py
+++ examples/texturetest.py
@@ -14,24 +14,30 @@ checkerboard = array([[0.,0.,1.,1.],
 					  [1.,1.,0.,0.]])
 lum = texture( data=checkerboard, type = "luminance")
 checkerboard = lum.data # in case a copy was made
-# By default, textures are mipmapped automatically.  The user should set it to
-# false if the texture will be changed frequently.
+# By default, textures are mipmapped automatically. The programmer should set
+# it to false if the texture will be changed frequently.
 alp = texture( mipmap = False, data=checkerboard, type = "opacity")
-box( color=color.orange, texture=alp)
-box( color=color.orange, texture=lum, pos=(-2, 0))
+balp1 = box( color=color.orange, texture=alp)
+blum1 = box( color=color.orange, texture=lum, pos=(-2, 0))
 box( pos=(0, 2), color=color.orange, opacity=0.5)
 box( pos=(0,-2), color=color.orange)
-box( pos=(0, 0, -2), color=color.orange, lit=False, texture=lum)
+blum2 = box( pos=(0, 0, -2), color=color.orange, lit=False, texture=lum)
 z = arrow()
 z.axis *= 2
 
-# Changes in the display are a consequence of changing the underlying data.
 scene.mouse.getclick()
-print "click!"
+# Change the texture
 save = array(checkerboard[:,1])
 checkerboard[:,1] = checkerboard[:,2]
 checkerboard[:,2] = save
 save = array(checkerboard[1,:])
 checkerboard[1,:] = checkerboard[2,:]
 checkerboard[2,:] = save
+# Recreate and reassign the textures; Visual doesn't check for texture changes
+lum = texture( data=checkerboard, type = "luminance")
+alp = texture( mipmap = False, data=checkerboard, type = "opacity")
+balp1.texture = alp
+blum1.texture = lum
+blum2.texture = lum
+
 
