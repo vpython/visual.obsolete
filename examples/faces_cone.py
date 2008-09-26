091db079d721ddab1a35ca8e6ca8f71c1ebf5bc0
bsherwood <bsherwood>
2008-09-26 02:31:04 +0000

    Eliminate undocumented smooth shading option; make it work with Numeric as well as numpy; adjust coloring

--- examples/faces_cone.py
+++ examples/faces_cone.py
@@ -10,7 +10,10 @@ box( size=(0.5,0.5,0.5) )
 
 N = 20
 
-model = faces( pos = zeros( (N*3,3), float ), frame = f )
+try: # numpy
+    model = faces( pos = zeros( (N*3,3), float ), frame = f )
+except: # older Numeric
+    model = faces( pos = zeros( (N*3,3), Float ), frame = f )
 
 t = arange(0,2*pi+2*pi/N,2*pi/N)
 
@@ -25,14 +28,19 @@ model.pos[1::3, 2] = cos(t[1:])
 # Vertex 2 of triangles (point of cone)
 model.pos[2::3, 1] = 2
 
-# All normals point outward in XZ... not quite right, but there is already a cone primitive :)
+# Construct normals to the edges of the triangles
 model.normal = model.pos
-model.normal[2::3,0] = sin((t[:-1]+t[1:])/2)
-model.normal[2::3,1] = 0
-model.normal[2::3,2] = cos((t[:-1]+t[1:])/2)
+model.normal[2::3,0] = sin(t[1:])
+model.normal[0::1,1] = 0.5
+model.normal[2::3,2] = cos(t[1:])
 
-model.color = model.pos/2 + (0.5,0,0.5)
-model.smooth_shade(doublesided=0)
+# Give each face a hue running from 0 to 1
+model.color = model.pos
+for vertex in range(3*N):
+    hue = float(vertex // 3)/N
+    model.color[vertex] = color.hsv_to_rgb((hue,1,1))
+
+##model.color = model.pos/2 + (0.5,0,0.5) # alternative coloring
 
 ##model.color[0::3] = (1,1,1)   # show where the triangles are
 
