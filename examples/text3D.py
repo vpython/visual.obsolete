b855903bc00573130559fd2709be37a9cb7c6145
bsherwood <bsherwood>
2010-04-08 21:31:27 +0000

    Implement multiline text; also Kadir Haldenbilen has sped up plotting somewhat

--- examples/text3D.py
+++ examples/text3D.py
@@ -1,25 +1,30 @@
 from visual import *
 
-scene.height = 400
+scene.height = 500
 scene.width = 800
 scene.range = 4
 scene.autocenter = True
-title = text(text='My Text', align='center', depth=-.3, color=color.cyan)
+title = text(text="My text is\ngreen", align='center', depth=-0.3, color=color.green)
 vertical = (title.upper_left-title.start).norm()
 height = curve(color=color.yellow, radius=0.02,
                pos=[title.start-.2*title.axis,
                     title.start-.2*title.axis+title.height*vertical])
 lheight = label(pos=.5*(height.pos[0]+height.pos[1]),
-                xoffset=-40, yoffset=+10, text='height')
+                xoffset=-30, yoffset=+10, text='height')
 descent = curve(color=color.yellow, radius=0.02,
                pos=[title.start+0.2*title.axis,
                     title.start+0.2*title.axis-title.descent*vertical])
 ldescent = label(pos=.5*(descent.pos[0]+descent.pos[1]),
-                xoffset=30, yoffset=-20, text='descent')
+                xoffset=25, yoffset=-20, text='descent')
 width = curve(color=color.yellow, radius=0.02,
                pos=[title.upper_left+0.1*vertical, title.upper_right+0.1*vertical])
 lwidth = label(pos=.5*(width.pos[0]+width.pos[1]),
                 xoffset=10, yoffset=40, text='width')
+verticalspacing = curve(color=color.yellow, radius=0.02,
+               pos=[title.start+title.widths[0]*title.axis,
+                    title.start+title.widths[0]*title.axis-vector(0,title.vertical_spacing,0)])
+lverticalspacing = label(pos=.5*(verticalspacing.pos[0]+verticalspacing.pos[1]),
+                xoffset=-30, yoffset=25, text='vertical_spacing')
 ul = label(pos=title.upper_left, text='upper_left',
            xoffset=-30, yoffset=30)
 ur = label(pos=title.upper_right, text='upper_right',
@@ -29,7 +34,7 @@ lr = label(pos=title.lower_right, text='lower_right',
 ll = label(pos=title.lower_left, text='lower_left',
            xoffset=-30, yoffset=-30)
 lc = label(text="pos (align='"+title.align+"')",
-           xoffset=10, yoffset=-40)
+           xoffset=10, yoffset=-20)
 ls = label(pos=title.start, text='start',
            xoffset=-60, yoffset=-30)
 for s in [ul, ur, lr, ll, lc, ls]:
