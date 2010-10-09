a203fedd6f3aabef8345b2cc5fa309b85fe06cd8
bsherwood <bsherwood>
2010-10-09 21:33:54 +0000

    Change print to print()

--- compilevisual.py
+++ compilevisual.py
@@ -2,10 +2,10 @@ from visual.graph import *
 from visual.controls import *
 from visual.filedialog import *
 from visual.factorial import *
-text(text="3D", color=color.red, material=materials.wood)
 from visual.text import *
-print factorial(4)
+print(factorial(4))
 get_file()
+text(text="3D", color=color.red, material=materials.wood)
 
 # Run this little program to compile all the visual modules.
 # This is part of the production of a VPython installer for Windows.
